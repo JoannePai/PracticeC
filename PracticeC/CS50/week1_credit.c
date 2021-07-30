1445a4705f77cb45497e1e4200d217fdbf0c6aab
JoannePai <71302866+JoannePai@users.noreply.github.com>
2021-07-30 17:28:55 +0800

    Add files via upload

---
+++ PracticeC/CS50/week1_credit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    int num;
+    printf("Credit card number:");
+    scanf("%f",&n);
+
+
+printf("Credit card number:%d\n",n);
+    num=sizeof(n);
+    printf("Credit card number:%d\n",num);
+
+}
